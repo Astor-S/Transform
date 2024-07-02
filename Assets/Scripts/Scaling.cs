700459f311e400d5db709d9a2353c24aa6cfbc30
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-07-02 22:13:44 +0300

    Vectore.one

--- Assets/Scripts/Scaling.cs
+++ Assets/Scripts/Scaling.cs
@@ -6,6 +6,6 @@ public class Scaling : MonoBehaviour
 
     private void Update()
     {
-        transform.localScale += new Vector3(_speed, _speed, _speed) * Time.deltaTime;
+        transform.localScale += Vector3.one * _speed * Time.deltaTime;
     }
 }
